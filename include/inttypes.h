efd819be2c9af1aa6c489e103c5de0ff7f5030da
Philipp Brüschweiler <blei42@gmail.com>
2012-05-22 15:24:19 +0200

    Initial commit, malloc and free compile (not tested yet)

---
+++ include/inttypes.h
@@ -0,0 +1,84 @@
+/* Copyright (c) 2004,2005,2007 Joerg Wunsch
+   Copyright (c) 2005, Carlos Lamas
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+
+   * Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+
+   * Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in
+     the documentation and/or other materials provided with the
+     distribution.
+
+   * Neither the name of the copyright holders nor the names of
+     contributors may be used to endorse or promote products derived
+     from this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE. */
+
+/* $Id: inttypes.h 1766 2008-10-17 21:33:57Z arcanum $ */
+
+#ifndef __INTTYPES_H_
+#define __INTTYPES_H_
+
+#include "stdint.h"
+
+/** \file */
+/** \defgroup avr_inttypes <inttypes.h>: Integer Type conversions
+    \code #include <inttypes.h> \endcode
+
+    This header file includes the exact-width integer definitions from
+    <tt><stdint.h></tt>, and extends them with additional facilities
+    provided by the implementation.
+
+    Currently, the extensions include two additional integer types
+    that could hold a "far" pointer (i.e. a code pointer that can
+    address more than 64 KB), as well as standard names for all printf
+    and scanf formatting options that are supported by the \ref avr_stdio.
+    As the library does not support the full range of conversion
+    specifiers from ISO 9899:1999, only those conversions that are
+    actually implemented will be listed here.
+
+    The idea behind these conversion macros is that, for each of the
+    types defined by <stdint.h>, a macro will be supplied that portably
+    allows formatting an object of that type in printf() or scanf()
+    operations.  Example:
+
+    \code
+    #include <inttypes.h>
+
+    uint8_t smallval;
+    int32_t longval;
+    ...
+    printf("The hexadecimal value of smallval is %" PRIx8
+           ", the decimal value of longval is %" PRId32 ".\n",
+	   smallval, longval);
+    \endcode
+*/
+
+/** \name Far pointers for memory access >64K */
+
+/*@{*/
+/** \ingroup avr_inttypes
+    signed integer type that can hold a pointer > 64 KB */
+typedef int32_t int_farptr_t;
+
+/** \ingroup avr_inttypes
+    unsigned integer type that can hold a pointer > 64 KB */
+typedef uint32_t uint_farptr_t;
+/*@}*/
+
+#endif /* __INTTYPES_H_ */
---
+++ include/stddef.h
@@ -0,0 +1,13 @@
+/*
+ * Some standard int types for the DCPU16.
+ */
+
+#pragma once
+
+#define NULL ((void*) 0)
+
+// TODO offsetof???
+
+typedef int ptrdiff_t;
+typedef unsigned size_t;
+typedef unsigned wchar_t;
